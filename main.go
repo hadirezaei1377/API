706d89e1dc33279a041e3b03fd7cc9970ba20a7b
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-01 23:04:24 +0330

    code for server

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"toplearn-golang-api/config"
+	"toplearn-golang-api/routing"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	// config
+	err := config.GetConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Server port : ", config.AppConfig.Server.Port)
+
+	//init server
+	server := echo.New()
+
+	//routing
+	routing.SetRouting(server)
+
+	//middleware
+
+	//start server
+	server.Start(":" + config.AppConfig.Server.Port)
+}
