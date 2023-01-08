19605c0fd8b72741cd8db84841b5ba68ca4683f7
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-08 23:29:14 +0330

    user list

---
+++ controller/basicuserControllerimproved.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"net/http"
+	"os/user"
+
+	"github.com/labstack/echo/v4"
+)
+
+func GetUserList(c echo.Context) error {
+
+	UserList := []user.User{
+		{
+			FirstName:   "Hadi",
+			LastName:    "Rezaei",
+			Age:         24,
+			PhoneNumber: "09131111111",
+		},
+		{
+			FirstName:   "Khashayar",
+			LastName:    "Rezaei",
+			Age:         35,
+			PhoneNumber: "09911111111",
+		},
+		{
+			FirstName:   "Gisoo",
+			LastName:    "Sotoodeh",
+			Age:         21,
+			PhoneNumber: "09351111111",
+		},
+	}
+	return c.JSON(http.StatusOK, userList, "")
+
+}
