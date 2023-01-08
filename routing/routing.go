8ee8d7490104a5f274b783e97da1c4c1e509daa1
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-08 22:17:50 +0330

    generalmod in routing

--- routing/routing.go
+++ routing/routing.go
@@ -9,31 +9,33 @@ import (
 
 func SetRouting(e *echo.Echo) error {
 
-	// What address did I get, what should I do?
-	e.GET("/users/get/:id", func(c echo.Context) error {
-		// send this response to client
+	// using groups instead of each parameters like users
+	g := e.Group("users")
+
+	myfunction := func(c echo.Context) error {
 		return c.String(http.StatusOK, "/users/get/   with param")
-		// with param shows to client the using part
-	})
-
-	e.GET("/users/get/:id/avatars", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users/get/[with param]/avatars")
-	})
-
-	// for any params that client sends :
-	e.GET("/users/get/:id/avatars/*", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users/get/[with param]/avatars/*")
-	})
-
-	e.GET("/users/get/15", func(c echo.Context) error {
-		return c.String(http.StatusOK, "/users/get/15")
-	})
-
-	// print routs
-	for i, route := range e.Routes() {
-		fmt.Println(i, route.Method, route.Path)
 	}
 
-	return nil
+	g.GET("/get/:id", myfunction).Name = "getUserById"
+	g.GET("/get/:id/avatars", getUserAvatar)
+	g.GET("/get/:id/avatars/*", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users/get/[with param]/avatars/*")
+	})
+	g.GET("/get/15", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users/get/15")
+	}).Name = "getUserNo15" // the name that we want
 
+	for i, route := range e.Routes() {
+		fmt.Println(i, route.Method, route.Path, route.Name)
+	}
+
+	fmt.Println(e.URI(getUserAvatar, 17))
+
+	fmt.Println(e.Reverse("getUserNo15", nil))
+	fmt.Println(e.Reverse("getUserById", 13))
+
+	return nil
+}
+func getUserAvatar(c echo.Context) error {
+	return c.String(http.StatusOK, "/users/get/[with param]/avatars")
 }
