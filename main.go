128ed91406d5d2afb0d93a57de65503a84161c9a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-08 16:03:19 +0330

    routing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"API/config"
+	"API/routing"
 	"fmt"
 	"log"
 
@@ -30,7 +31,7 @@ func main() {
 	*/
 
 	// routing
-	// routing.SetRouting(server)
+	routing.SetRouting(server)
 
 	// start server or implement it
 	server.Start(":" + config.AppConfig.Server.Port)
