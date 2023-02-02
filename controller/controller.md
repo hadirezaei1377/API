e777f61776d63bab20e17a10ae0df8e739c6cec9
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-02-02 15:37:02 +0330

    pipeline intro for improvemement

---
+++ controller/controller.md
@@ -0,0 +1,4 @@
+In this section, we only determine the project process. 
+Where requests come from, where to go and how to respond(in general the pipeline of project and doing filter for response)
+
+
--- service/UserService.go
+++ service/UserService.go
@@ -1,37 +1,44 @@
 package service
 
+// service for improrve the Project architecture
+
 import "API/model/user"
+
+// an user can user interface for using interface but not the main userservice
 
 type UserService interface {
 	GetUserList() ([]user.User, error)
 }
 
+// private struct for main userservice
 type userService struct {
 }
 
+// return new user service
 func NewUserService() UserService {
 	return userService{}
 }
 
+// function for organize the list of users instead controller
 func (userService) GetUserList() ([]user.User, error) {
 	userList := []user.User{
 		{
-			FirstName:   "Mohammad",
-			LastName:    "Ghari",
-			Age:         27,
-			PhoneNumber: "0912345678",
+			FirstName:   "Hadi",
+			LastName:    "Rezaei",
+			Age:         24,
+			PhoneNumber: "09131111111",
 		},
 		{
-			FirstName:   "Ali",
-			LastName:    "Rezae",
-			Age:         26,
-			PhoneNumber: "0913345678",
+			FirstName:   "Khashayar",
+			LastName:    "Rezaei",
+			Age:         35,
+			PhoneNumber: "09911111111",
 		},
 		{
-			FirstName:   "Iman",
-			LastName:    "Madaeni",
-			Age:         32,
-			PhoneNumber: "0914345678",
+			FirstName:   "Gisoo",
+			LastName:    "Sotoodeh",
+			Age:         21,
+			PhoneNumber: "09351111111",
 		},
 	}
 
