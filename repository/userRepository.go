1b28bbc20fe70a109eb2f523a6e0de1d023457ea
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-02-04 10:47:38 +0330

    read from CRUD

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -19,6 +19,9 @@ type userRepository struct {
 	db database.Db
 }
 
+// read from db
+// read section of CRUD operation
+
 func NewUserRepository() UserRepository {
 	db, err := database.Connect()
 	if err != nil {
@@ -52,4 +55,38 @@ func (userRepository userRepository) GetUserList() ([]user.User, error) {
 }
 
 
+*/
+
+// Read section of CRUd related to db
+/*
+
+func (userRepository userRepository) GetUserById(id string) (user.User, error) {
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return user.User{}, err
+	}
+	userCollection := userRepository.db.GetUserCollection()
+	var userObject user.User
+	// read codes from MongoDB documantation
+	// there is BsonD, BsonA, BsonE, ...
+	// its about orders,
+    // When using it, it asks if the order is important to you or not
+    // we have document so we use bsonD
+    // The order is not important for us and we just want our ID to be the same as the ID in the database.
+
+	// db.getCollection('users').find({"_id" : ObjectId("6297bdcc8d7757574658ed66")})
+
+	// _id is key for map
+	err = userCollection.FindOne(context.TODO(), bson.D{
+		{"_id", objectId},
+	}).Decode(&userObject)
+
+	if err != nil {
+		return user.User{}, err
+	}
+
+	return userObject, nil
+
+}
 */
