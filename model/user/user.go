ae8f0a0e44fced9a42d6699691003dd1927d12b7
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-02-04 10:06:21 +0330

    bson and repository in db

--- model/user/user.go
+++ model/user/user.go
@@ -1,9 +1,25 @@
 package user
 
+// normal mod
+/*
+
 // related struct to user
 type User struct {
 	FirstName   string
 	LastName    string
 	Age         int
 	PhoneNumber string
+}
+
+*/
+
+// related to db connection
+// db uses bson
+
+type User struct {
+	Id          string `bson:"_id,omitempty"`
+	FirstName   string `bson:"firstName,omitempty"`
+	LastName    string `bson:"lastName,omitempty"`
+	Age         int    `bson:"age,omitempty"`
+	PhoneNumber string `bson:"phonenumber,omitempty"`
 }
