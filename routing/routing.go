937e3c641cdcbb80513743a50626860b012cf2f7
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-03-27 14:29:47 +0330

    two another middlewares

--- routing/routing.go
+++ routing/routing.go
@@ -8,11 +8,32 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// group level middlewares
+func GroupLevel(next echo.HandlerFunc) echo.HandlerFunc {
+	fmt.Println("GroupLevel")
+	return next
+}
+
+// route level middlewares
+// related to get and post in bottom of this file
+func RouteLevel(next echo.HandlerFunc) echo.HandlerFunc {
+	fmt.Println("RouteLevel")
+	return next
+}
+func RouteLevel2(next echo.HandlerFunc) echo.HandlerFunc {
+	fmt.Println("RouteLevel2")
+	return next
+}
+func RouteLevel3(next echo.HandlerFunc) echo.HandlerFunc {
+	fmt.Println("RouteLevel3")
+	return next
+}
+
 func SetRouting(e *echo.Echo) error {
 
-	g := e.Group("users")
+	g := e.Group("users", GroupLevel)
 
-	g.GET("/getList", controller.GetUserList)
+	g.GET("/getList", controller.GetUserList, RouteLevel, RouteLevel2, RouteLevel3)
 	g.POST("/CreateNewUser", controller.CreateNewUser)
 
 	return nil
