8e9a6d242e1ed28b1e3af2e0b6756def69cacffd
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-07-24 17:25:47 +0330

    use middlewares for controlling roles

--- routing/routing.go
+++ routing/routing.go
@@ -1,8 +1,11 @@
 package routing
 
 import (
+	"API/Utility"
 	"API/ViewModel/common/security"
 	"API/controller"
+	"API/service"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -20,7 +23,34 @@ func SetRouting(e *echo.Echo) error {
 		SigningKey: []byte("secret"),
 		Claims:     &security.JwtClaims{},
 	}
-	g.POST("/CreateNewUser", controller.CreateNewUser, middleware.JWTWithConfig(jwtConfig))
+	g.POST("/CreateNewUser", controller.CreateNewUser, PermissionChecker("CreateNewUser"), middleware.JWTWithConfig(jwtConfig))
 
 	return nil
+}
+func PermissionChecker(permission string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			apiContext := c.(*Utility.ApiContext)
+
+			operatorUserId, err := apiContext.GetUserId()
+			if err != nil {
+				return &echo.HTTPError{
+					Code:     401,
+					Message:  http.StatusUnauthorized,
+					Internal: err,
+				}
+			}
+
+			userService := service.NewUserService()
+			isValid := userService.IsUserValidForAccess(operatorUserId, permission)
+			if !isValid {
+				return &echo.HTTPError{
+					Code:    403,
+					Message: http.StatusForbidden,
+				}
+			}
+
+			return next(c)
+		}
+	}
 }
